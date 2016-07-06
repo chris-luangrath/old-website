1910911aa40e16379936cdde3672bb7837ce3f73
Christian Luangrath <thechrisluangrath@gmail.com>
2016-07-06 16:45:09 +0000

    Adding js to start having one header html file

---
+++ active/activeProjects.js
@@ -0,0 +1,11 @@
+var aTags = document.getElementsByTagName("button");
+var searchText = "Projects";
+var found;
+
+for (var i = 0; i < aTags.length; i++) {
+  if (aTags[i].textContent == searchText) {
+      aTags[i].id = "active";
+  } else {
+      aTags[i].id = "inactive";
+  }
+}
