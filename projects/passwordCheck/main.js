45125d16dcc8f6d1a6e230e1d20868d279e97f7e
Christian Luangrath <thechrisluangrath@gmail.com>
2016-07-12 18:02:52 +0000

    pretty much works now, just have to tidy things up

--- projects/passwordCheck/main.js
+++ projects/passwordCheck/main.js
@@ -1,12 +1,16 @@
 window.onload = function() { //This makes the list from the input
     var input = document.getElementById('input');
     var reply = document.getElementById('reply');
-    input.addEventListener('keypress', function () {
+    input.addEventListener('keydown', function (e) {
+        var key = e.which || e.keyCode;
         if ((input.value).length < 2) { // 13 is enter
         //input has value, div has innerHTML
             reply.innerHTML = "My dog could do better";
-        } else {
-            reply.innerHTML = "Good enough for now";
+        } else if ((input.value).length < 2) { // 13 is enter
+            reply.innerHTML = "My dog could do better";
+        } else if ((input.value).length < 2) { // 13 is enter
+        //input has value, div has innerHTML
+            reply.innerHTML = "My dog could do better";
         }
     });
 };
