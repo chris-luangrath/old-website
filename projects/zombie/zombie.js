4ffbe069abd56cc328c4983a9fd4d608fbf2326e
Christian Luangrath <thechrisluangrath@gmail.com>
2016-07-15 06:16:55 +0000

    zombie has started

--- projects/zombie/zombie.js
+++ projects/zombie/zombie.js
@@ -1,24 +1,26 @@
+window.onload = function() {
+        var input, output;
+        input = document.getElementBy
+        
+        alert("The year is 20ZZ. Zombies exist now. It's kinda scary");
+        var go = false;
+        do {
+            action = prompt("You are walking down the street when suddenly, you see a zombie walking towards you! You can: walk the other way, strike up a conversation, or punch him in the schnozz. What will you do: walk, talk, or fight?");
+            if (action == "walk") {
+                alert("Lucky for you, zombies can't walk fast. You hastily speed walk away from danger and you live another day. You Win!");
+                go = true;
+            } else if (action == "talk") {
+                alert("You stop what you're doing and start talking to the zombie. It turns out, zombies don't eat brains. It was just a racial stereotype that early Americans created. It turns out, the zombie's name is Tom, and he wants to be a software engineer. Good for him. Good for you. You Win.");
+                go = true;
+            } else if (action == "fight") {
+                alert("You feel a surge of adrenalin as you stop walking towards the zombie. You will your hand to burst into flames and you run towards the zombie and deck him in the face. He falls down and starts to cry about \"How much it hurts\" and \"how he has a family\". Soon, the zombie is no more. You Win?");
+                go = true;
+            } else if (action == "walk, talk, or fight") {
+                alert("OH YOU THINK YOU'RE SOO CLEVER, EH BOY?");
+                go = true;
+            } else {
+                alert("type walk, talk, or fight. Lets try again.");
+            }
 
-var START = {
-alert("The year is 20ZZ. Zombies exist now. It's kinda scary");
-var go = false
-do{
-  action = prompt("You are walking down the street when suddenly, you see a zombie walking towards you! You can: walk the other way, strike up a conversation, or punch him in the schnozz. What will you do: walk, talk, or fight?");
-  if(action == "walk" ){
-   alert("Lucky for you, zombies can't walk fast. You hastily speed walk away from danger and you live another day. You Win!");
-   go = true
-  } else if (action == "talk"){
-    alert("You stop what you're doing and start talking to the zombie. It turns out, zombies don't eat brains. It was just a racial stereotype that early Americans created. It turns out, the zombie's name is Tom, and he wants to be a software engineer. Good for him. Good for you. You Win.");
-    go = true
-  } else if (action == "fight"){
-    alert("You feel a surge of adrenalin as you stop walking towards the zombie. You will your hand to burst into flames and you run towards the zombie and deck him in the face. He falls down and starts to cry about \"How much it hurts\" and \"how he has a family\". Soon, the zombie is no more. You Win?");
-    go = true
-  } else if (action == "walk, talk, or fight"){
-    alert("OH YOU THINK YOU'RE SOO CLEVER, EH BOY?");
-    go = true
-  } else {
-    alert("type walk, talk, or fight. Lets try again.")      
-  }
-          
-} while (go == false);
-}
+        } while (go === false);
+    };
